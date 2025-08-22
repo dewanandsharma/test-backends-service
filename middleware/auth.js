be9b0f3ae0ca84c441cbbd2d06d9769e2dfb8acb
Dewanand Sharma <dewanandsharma143@gmail.com>
2025-08-22 12:06:39 -0400

    added nodeModule

---
+++ middleware/auth.js
@@ -0,0 +1,7 @@
+// middleware/auth.js
+export default (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next();
+  }
+  return res.status(401).json({ message: 'Unauthorized' });
+};
